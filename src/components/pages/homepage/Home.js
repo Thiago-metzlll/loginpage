b4139cbe34f6d18527e945e7f6d51193062f85c0
Thiago-metzlll <thiagostopassola@gmail.com>
2025-06-03 21:33:34 -0300

    att

--- src/components/pages/homepage/Home.js
+++ src/components/pages/homepage/Home.js
@@ -1,5 +1,4 @@
 // src/components/CartaoListaVirtual.jsx
-import { FixedSizeList } from "react-window";
 import styles from './Home.module.scss';
 import { FixedSizeGrid as Grid } from "react-window";
 
@@ -25,7 +24,9 @@ function Home({ alturaCartao = 120, larguraCartao = 250 }) {
           className={styles.Card}
         >
           <h4>{cartao.nome}</h4>
-          <p>ID: {cartao.id}</p>
+          <img src="https://images.unsplash.com/photo-1741850821875-926bc6332d97?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTd8fHxlbnwwfHx8fHw%3D"
+            alt="Imagem do cartão" className={styles.cardImage} />
+
         </div>
       </div>
     );
